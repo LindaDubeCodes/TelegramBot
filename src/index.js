944329679b3a06953353d2efd8cb32a624aa6e15
cumPirate <crazyrealindividual123@gmail.com>
2023-04-07 21:10:45 +1400

    using telegraf

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const { Telegraf } = require('telegraf')
 
 
 const token = process.env.TOKEN;
 
+const bot = new Telegraf(token);
 
-const bot = new TelegramBot(token, { polling: true });
+const helpText =
+    `
+Say something to me
+/start - start the bot
+/help - command reference
+`;
 
+bot.start((ctx) => {
+    ctx.reply(ctx.from.first_name + " " + "You have entered the start command and it is a ");
+    ctx.reply(helpText);
 
-bot.on('message', (msg) => {
-
-    let chat_id = msg.from.id || msg.from.first_name;
-
-    bot.sendMessage(chat_id, "guns and condoms");
-});
-
-bot.command('start', (ctx) => {
 
 })
+
+bot.help((ctx) => {
+    ctx.reply("You have entered the help command");
+})
+
+bot.hears("history", (ctx) => {
+    ctx.reply("Ahh you want hitler huh....You dirty bastard!");
+})
+bot.launch()
